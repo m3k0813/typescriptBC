49b7a8a36bf3ef82fe5a989c77a629c9519df4f4
m3k0813 <m3k0813@gmail.com>
2021-03-20 20:44:18 +0900

    class human

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-    return 'Hello ${name}, you are ${age}, you are a ${gender}!';
+class Human {
+    public name: string;
+    private age: number;
+    public gender: string;
+    constructor(name: string, age:number, gender:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-console.log(sayHi("JeongJun", 20, "male"));
+const lynn = new Human("Lynn", 18, "female");
 
-export {};
+  const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${
+      person.gender
+    }!`;
+  };
+  
+  console.log(sayHi(lynn));
+  
+  export {};
